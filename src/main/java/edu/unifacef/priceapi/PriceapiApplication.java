cc405ae101f5e213c61746f701bd833c7b7477c7
odair-pedroso <odairpedroso90@gmail.com>
2022-01-02 14:56:16 -0300

    add inicial

---
+++ src/main/java/edu/unifacef/priceapi/PriceapiApplication.java
@@ -0,0 +1,13 @@
+package edu.unifacef.priceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/unifacef/priceapi/PriceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unifacef.priceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
