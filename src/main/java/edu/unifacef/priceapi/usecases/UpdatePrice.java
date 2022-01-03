d5facbba21106be442ef28760a830ae53ac348e6
odair-pedroso <odairpedroso90@gmail.com>
2022-01-03 16:59:03 -0300

    add UpdatePrice

--- src/main/java/edu/unifacef/priceapi/usecases/UpdatePrice.java
+++ src/main/java/edu/unifacef/priceapi/usecases/UpdatePrice.java
@@ -3,8 +3,8 @@ package edu.unifacef.priceapi.usecases;
 import org.ff4j.FF4j;
 import org.springframework.stereotype.Component;
 
+import edu.unifacef.priceapi.gateways.outputs.LocationGateway;
 import edu.unifacef.priceapi.gateways.outputs.PriceDataGateway;
-
 import edu.unifacef.priceapi.utils.MessageUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +13,16 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @RequiredArgsConstructor
 public class UpdatePrice {
+	
+	private final PriceDataGateway priceDataGateway;
+	private final LocationGateway locationGateway;
+	private final MessageUtils messageUtils;
+	private final FF4j ff4j;
+	
+	
+	  
+	  
+	  
 	
 	
 
